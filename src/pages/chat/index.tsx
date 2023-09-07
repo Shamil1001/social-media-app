299e829e7936993492fe5e9c68b37eab6e9d2555
Shamil <shjemhurov@gmail.com>
2023-09-07 23:32:04 +0500

    sidebar fixing 2

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,7 +1,7 @@
 import Chatbox from "@/components/chatbox";
 import Navbar from "@/components/navbar/navbar";
 // import Sidebar from "../../components/sidebar/index";
-import Sidebar from "../../components/sidebar/index";
+import Sidebar from "../../components/sidebar/sidebar";
 import { Box } from "@chakra-ui/react";
 
 export default function Chat() {
